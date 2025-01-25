4ec0478f01f81e8dec718747946862fe73cb041b
Sooparkle <imsoohanmu@gmail.com>
2025-01-25 21:16:36 +0900

    Style(Loading) : Modified styles

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,10 +1,14 @@
 // app/notifications/loading.tsx
+import styles from '@/app/styles/loading.module.css'
 
-export default function Loading() {
+export default function NotificationLoading() {
+
+
   return (
-    <div className="loading-container">
-      <div className="loading-spinner" />
-      <p>로딩 중...</p>
+    <div className={styles.loadingContainer}>
+      <div className={styles.loadingSpinner} />
+      <h2 className={styles.title}>...로딩 중...</h2>
+      <p className={styles.message}>게시판에 데이터를 가져오고 있습니다.</p>
     </div>
-  )
+  );
 }
--- app/notifications/loading.tsx
+++ app/notifications/loading.tsx
@@ -1,11 +1,14 @@
 // app/notifications/loading.tsx
+import styles from '@/app/styles/loading.module.css'
+
+export default function NotificationLoading() {
 
 
-export default function NotificiationLoading() {
   return (
-    <div className="notification-loading-container">
-      <div className="loading-spinner" />
-      <p>로딩 중...</p>
+    <div className={styles.loadingContainer}>
+      <div className={styles.loadingSpinner} />
+      <h2 className={styles.title}>...로딩 중...</h2>
+      <p className={styles.message}>게시판에 데이터를 가져오고 있습니다.</p>
     </div>
-  )
+  );
 }
