49eb727085692939ed2aecc44d858c7916c24b0a
Sooparkle <imsoohanmu@gmail.com>
2025-01-27 21:53:41 +0900

    Style(Main) : Added images and CSS

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,16 @@ import styles from "./page.module.css";
 import Link from "next/link";
 
 export default function Home() {
+
+  const feature = {
+    1 : "주간 달력",
+    2 : "기록 상세",
+    3 : "새로운 기록",
+    4 : "기록 수정"
+  }
+
   return (
+    <>
     <main
     className={styles.main}
     >
@@ -14,11 +23,6 @@ export default function Home() {
         <div
           className={styles.heroContent}
         >
-          <h1 className={styles.tit}>당신이 마는 차를 기록해 보세요.</h1>
-          <p>
-            이번 달에 나는 어떤 차를 마셨다라? 궁금하거나<br/>
-            나는 어떤 맛을 느꼈지? 천천히 알아가고 싶다면.
-          </p>
 
         <div
           className={styles.heroImages}
@@ -27,28 +31,42 @@ export default function Home() {
             className={styles.heroImageContainer}
           >
             <Image 
-              src="/images/main.png"
+              src="/images/main1.png"
               alt="대표 앱 이미지"
               fill
-              sizes="(max-width : 350px)"
+              sizes="(max-width : 700px)"
               objectFit="contain"
               priority
+              className={styles.mainImage}
             />
+
           </div>
           <div
             className={styles.heroImageContainer}
           >
+
             <Image 
-              src="/images/main.png"
+              src="/images/main2.png"
               alt="대표 앱 이미지"
               fill
-              sizes="(max-width : 350px)"
+              sizes="(max-width : 700px)"
               objectFit="contain"
               priority
             />
           </div>
         </div>
-          
+
+        <div
+          className={styles. heroImageText}
+        >
+          <h1 className={styles.tit}>당신이 마시는 차를 기록해 보세요.</h1>
+          <p>
+            이번 달에 나는 어떤 차를 마셨다라? 궁금하거나<br/>
+            나는 어떤 맛을 느꼈지? 천천히 알아가고 싶다면.
+          </p>
+
+          </div>
+
         </div>
       </section>
 
@@ -63,19 +81,19 @@ export default function Home() {
           <article
             className={styles.featureCard}
           >
-            <h3>주간/월간 달력</h3>
+            <h3>주간/월간 달력 변경 🗓️</h3>
             <p>편리하게 원하는 날짜에 기록을 정리할 수 있다.</p>
           </article>
           <article
             className={styles.featureCard}
           >
-            <h3>향미 추가</h3>
+            <h3>다양한 향미 추가 👅</h3>
             <p>생각나지 않는 향미를 편리하게 추가할 수 있다.</p>
           </article>
           <article
             className={styles.featureCard}
           >
-            <h3>빠른 최근 기록 찾기</h3>
+            <h3>빠른 최근 기록 찾기 🔍</h3>
             <p>최근 기록한 정보를 메인 페이지서 단번에 찾아갈 수있다.</p>
           </article>
         </div>
@@ -90,51 +108,66 @@ export default function Home() {
         <div
           className={styles.screenshotImage}
         >
-          {
-            [1,2,3].map(i => (
-              <div
-                key={i}
-                className={styles.imagedContainer}
-              >
-                <Image
-                  src={`/images/modified${i}.png`}
-                  alt={`이미지${i}`}
-                  fill
-                  sizes="width: 350px"
-                  objectFit="contain"
-                />
-              </div>
-            ))
-          }
+          {/* <div> */}
+            {
+              Object.entries(feature).map(([key, value]) =>(
+              
+                <div
+                  key={key}
+                  className={styles.imagedContainer}
+                >
+                  <Image
+                    src={`/images/feature${key}.png`}
+                    alt={`${value}`}
+                    // fill
+                    // sizes="width: 350px"
+                    width={276}
+                    height={600}
+                    objectFit="contain"
+                    className={styles.screenshotItemImage}
+                  />
+                  <div>
+                    <p>{`${value}`}</p>
+                  </div>
+                </div>
+              ))
+
+            }
+          {/* </div> */}
 
         </div>
       </section>
 
       <section className={styles.download}>
         <h2>지금 바로 시작하세요</h2>
-        <p>iOS와 Android에서 모두 만나보실 수 있습니다</p>
+        <p>iOS와 Android에서 모두 만나보실 수 있습니다.</p>
         <div className={styles.downloadButtons}>
-          <Link href="#" className={styles.downloadButton}>
+          <p
+            className={styles.downloadText}
+          >준비중</p>
+          {/* <Link href="#" className={styles.downloadButton}>
           <p>Apple</p>
-            {/* <Image
+            <Image
               src="/api/placeholder/200/60"
               alt="App Store에서 다운로드"
               width={200}
               height={60}
-            /> */}
+            />
           </Link>
           <Link href="#" className={styles.downloadButton}>
           <p>android</p>
-            {/* <Image
+            <Image
               src="/api/placeholder/200/60"
               alt="Google Play에서 다운로드"
               width={200}
               height={60}
-            /> */}
-          </Link>
+            />
+          </Link> */}
         </div>
       </section>
 
     </main>
+    </>
+    
   );
 }
